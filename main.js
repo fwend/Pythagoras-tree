f2ae2043aaec6ef517403dec01a1547a0f0e453c
fwend <josvanuden@gmail.com>
2021-12-14 13:01:07 +0100

    refactor

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-var canvas = document.querySelector('canvas');
+const canvas = document.querySelector('canvas');
 canvas.width = 832;
 canvas.height = 832;
 
-var g = canvas.getContext('2d');
+const g = canvas.getContext('2d');
 
-var depthLimit = 7;
-var hue = 0.15;
+const depthLimit = 7;
+const hue = 0.15;
 
 function drawTree(x1, y1, x2, y2, depth) {
 
     if (depth == depthLimit)
         return;
 
-    var dx = x2 - x1;
-    var dy = y1 - y2;
+    const dx = x2 - x1;
+    const dy = y1 - y2;
 
-    var x3 = x2 - dy;
-    var y3 = y2 - dx;
-    var x4 = x1 - dy;
-    var y4 = y1 - dx;
-    var x5 = x4 + 0.5 * (dx - dy);
-    var y5 = y4 - 0.5 * (dx + dy);
+    const x3 = x2 - dy;
+    const y3 = y2 - dx;
+    const x4 = x1 - dy;
+    const y4 = y1 - dx;
+    const x5 = x4 + 0.5 * (dx - dy);
+    const y5 = y4 - 0.5 * (dx + dy);
 
     g.beginPath();
     g.moveTo(x1, y1);
@@ -51,7 +51,7 @@ function drawTree(x1, y1, x2, y2, depth) {
 
 /* copied from stackoverflow */
 function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
+    let r, g, b, i, f, p, q, t;
 
     i = Math.floor(h * 6);
     f = h * 6 - i;
@@ -59,12 +59,12 @@ function HSVtoRGB(h, s, v) {
     q = v * (1 - f * s);
     t = v * (1 - (1 - f) * s);
     switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
     }
     return "rgb("
         + Math.round(r * 255) + ","
